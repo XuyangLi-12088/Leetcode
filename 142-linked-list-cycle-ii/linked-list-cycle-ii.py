aa1b268a7b875330ccefb579b2ab8f624e4ac9c2
Xuyang Li <75594446+XuyangLi-12088@users.noreply.github.com>
2026-05-29 16:17:57 -0500

    Time: 44 ms (93.79%) | Memory: 22.5 MB (21.93%) - LeetSync

---
+++ 142-linked-list-cycle-ii/linked-list-cycle-ii.py
@@ -0,0 +1,33 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def detectCycle(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        # Fast and Slow Pointers:
+        slow = head
+        fast = head
+
+        while fast != None and fast.next != None:
+            slow = slow.next
+            fast = fast.next.next
+            if slow == fast:
+                slow = head
+                while slow != fast:
+                    slow = slow.next
+                    fast = fast.next
+                return slow
+
+        return None
+
+        # # Set Solutions:
+        # visited = set()
+        # cur = head
+        # while cur:
+        #     if cur in visited:
+        #         return cur
+        #     visited.add(cur)
+        #     cur = cur.next
+        # return None
