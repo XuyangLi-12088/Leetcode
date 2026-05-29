14af14a0084e427a5d008668cb13a0974322965f
Xuyang Li <75594446+XuyangLi-12088@users.noreply.github.com>
2026-05-28 22:58:13 -0500

    Time: 38 ms (62.37%) | Memory: 20.4 MB (33.06%) - LeetSync

---
+++ 224-basic-calculator/basic-calculator.py
@@ -0,0 +1,26 @@
+class Solution:
+    def calculate(self, s: str) -> int:
+        res, num, sign = 0, 0, 1
+        stack = []
+        for c in s:
+            if c.isdigit():
+                num = 10 * num + int(c)
+            elif c == "+" or c == "-":
+                res += sign * num
+                num = 0
+                if c == "+":
+                    sign = 1
+                else:
+                    sign = -1
+            elif c == "(":
+                stack.append(res)
+                stack.append(sign)
+                res = 0
+                sign = 1
+            elif c == ")":
+                res += sign * num
+                num = 0
+                res *= stack.pop()
+                res += stack.pop()
+        res += sign * num
+        return res
