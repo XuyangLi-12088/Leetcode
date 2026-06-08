ebf12cb56aa28810bf769dd2a5d9eb9790fc19fe
Xuyang Li <75594446+XuyangLi-12088@users.noreply.github.com>
2026-06-08 15:24:04 -0500

    Time: 15 ms (71.02%) | Memory: 21.4 MB (77.23%) - LeetSync

---
+++ 135-candy/candy.py
@@ -0,0 +1,21 @@
+class Solution:
+    def candy(self, ratings: List[int]) -> int:
+        ans = n = len(ratings)  # 先给每人分一个
+        i = 0
+        while i < n:
+            start = i - 1 if i > 0 and ratings[i - 1] < ratings[i] else i
+
+            # 找严格递增段
+            while i + 1 < n and ratings[i] < ratings[i + 1]:
+                i += 1
+            top = i # 峰顶
+
+            # 找严格递减段
+            while i + 1 < n and ratings[i] > ratings[i + 1]:
+                i += 1
+
+            inc = top - start # start 到 top 严格递增
+            dec = i - top   # top 到 i 严格递减
+            ans += (inc * (inc - 1) + dec * (dec - 1)) // 2 + max(inc, dec) # max(inc, dec)为峰顶的糖果数
+            i += 1
+        return ans
