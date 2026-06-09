5600921f04f0cbd1ba37984b1818375c70080b4a
Xuyang Li <75594446+XuyangLi-12088@users.noreply.github.com>
2026-06-09 15:15:55 -0500

    Time: 0 ms (100.00%) | Memory: 20.2 MB (92.95%) - LeetSync

---
+++ 110-balanced-binary-tree/balanced-binary-tree.py
@@ -0,0 +1,27 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    # 递归法:
+    def isBalanced(self, root: Optional[TreeNode]) -> bool:
+        if self.get_height(root) != -1:
+            return True
+        else:
+            return False
+
+    def get_height(self, root: Optional[TreeNode]) -> int:
+        if not root:
+            return 0
+        left_height = self.get_height(root.left)
+        if (left_height == -1):
+            return -1
+        right_height = self.get_height(root.right)
+        if (right_height == -1):
+            return -1
+        if abs(left_height - right_height) > 1:
+            return -1
+        else:
+            return max(left_height, right_height) + 1
