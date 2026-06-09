3e3289661b9919aa72f2f42d1b505459e89ab5eb
Xuyang Li <75594446+XuyangLi-12088@users.noreply.github.com>
2026-06-09 14:41:27 -0500

    Time: 3 ms (21.97%) | Memory: 20.2 MB (62.14%) - LeetSync

---
+++ 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.py
@@ -0,0 +1,34 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        # # Recursion Solution:
+        # if root is None:
+        #     return 0
+        # else:
+        #     left = self.maxDepth(root.left)
+        #     right = self.maxDepth(root.right)
+        #     return max(left, right) + 1
+
+
+        # BFS Solution:
+        if not root:
+            return 0
+        queue = deque()
+        queue.append(root)
+        result = 0
+        while queue:
+            cur_queue_size = len(queue)
+            for _ in range(cur_queue_size):
+                cur = queue.popleft()
+                if cur.left:
+                    queue.append(cur.left)
+                if cur.right:
+                    queue.append(cur.right)
+            result += 1
+        return result
+        
