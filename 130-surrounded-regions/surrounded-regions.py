19d8628471833c9d08977ba2a9d0faa773a87109
Xuyang Li <75594446+XuyangLi-12088@users.noreply.github.com>
2026-06-11 21:37:18 -0500

    Time: 3 ms (84.70%) | Memory: 22.4 MB (41.24%) - LeetSync

--- 130-surrounded-regions/surrounded-regions.py
+++ 130-surrounded-regions/surrounded-regions.py
@@ -3,41 +3,77 @@ class Solution:
         """
         Do not return anything, modify board in-place instead.
         """
-        # DFS:
+        # # DFS:
+        # if not board or not board[0]:
+        #     return
+        # row = len(board)
+        # col = len(board[0])
+
+        # def dfs(i, j):
+        #     board[i][j] = "B"
+        #     for x, y in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
+        #         tmp_i = i + x
+        #         tmp_j = j + y
+        #         if 1 <= tmp_i < row and 1 <= tmp_j < col and board[tmp_i][tmp_j] == "O":
+        #             dfs(tmp_i, tmp_j)
+
+        # for j in range(col):
+        #     # 第一行
+        #     if board[0][j] == "O":
+        #         dfs(0, j)
+        #     # 最后一行
+        #     if board[row - 1][j] == "O":
+        #         dfs(row - 1, j)
+
+        # for i in range(row):
+        #     # 第一列
+        #     if board[i][0] == "O":
+        #         dfs(i, 0)
+        #     # 最后一列
+        #     if board[i][col-1] == "O":
+        #         dfs(i, col - 1)
+
+        # for i in range(row):
+        #     for j in range(col):
+        #         # O 变成 X
+        #         if board[i][j] == "O":
+        #             board[i][j] = "X"
+        #         # B 变成 O
+        #         if board[i][j] == "B":
+        #             board[i][j] = "O"
+
+
+        # BFS:
         if not board or not board[0]:
             return
         row = len(board)
         col = len(board[0])
 
-        def dfs(i, j):
-            board[i][j] = "B"
-            for x, y in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
-                tmp_i = i + x
-                tmp_j = j + y
-                if 1 <= tmp_i < row and 1 <= tmp_j < col and board[tmp_i][tmp_j] == "O":
-                    dfs(tmp_i, tmp_j)
+        def bfs(i, j):
+            queue = deque()
+            queue.appendleft((i, j))
+            while queue:
+                i, j = queue.pop()
+                if 0 <= i < row and 0 <= j < col and board[i][j] == "O":
+                    board[i][j] = "B"
+                    for x, y in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
+                        queue.appendleft((i + x, j + y))
 
         for j in range(col):
-            # 第一行
             if board[0][j] == "O":
-                dfs(0, j)
-            # 最后一行
+                bfs(0, j)
             if board[row - 1][j] == "O":
-                dfs(row - 1, j)
+                bfs(row - 1, j)
 
         for i in range(row):
-            # 第一列
             if board[i][0] == "O":
-                dfs(i, 0)
-            # 最后一列
-            if board[i][col-1] == "O":
-                dfs(i, col - 1)
+                bfs(i, 0)
+            if board[i][col - 1] == "O":
+                bfs(i, col - 1)
 
         for i in range(row):
             for j in range(col):
-                # O 变成 X
                 if board[i][j] == "O":
                     board[i][j] = "X"
-                # B 变成 O
                 if board[i][j] == "B":
                     board[i][j] = "O"
