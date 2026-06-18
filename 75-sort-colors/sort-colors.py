a4025ec15e07dde9700437f47fe426a11972c9b6
Xuyang Li <75594446+XuyangLi-12088@users.noreply.github.com>
2026-06-18 16:29:52 -0500

    Time: 0 ms (100.00%) | Memory: 19.1 MB (89.85%) - LeetSync

--- 75-sort-colors/sort-colors.py
+++ 75-sort-colors/sort-colors.py
@@ -3,12 +3,25 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        p0 = p1 = 0
-        for i, x in enumerate(nums):
-            nums[i] = 2
-            if x <= 1:
-                nums[p1] = 1
-                p1 += 1
+        # p0 = p1 = 0
+        # for i, x in enumerate(nums):
+        #     nums[i] = 2
+        #     if x <= 1:
+        #         nums[p1] = 1
+        #         p1 += 1
+        #     if x == 0:
+        #         nums[p0] = 0
+        #         p0 += 1
+
+        nums0 = nums1 = nums2 = 0
+        for x in nums:
             if x == 0:
-                nums[p0] = 0
-                p0 += 1
+                nums0 += 1
+            elif x == 1:
+                nums1 += 1
+            elif x == 2:
+                nums2 += 1
+
+        nums[:nums0] = [0] * nums0
+        nums[nums0:nums0+nums1] = [1] * nums1
+        nums[nums0+nums1:] = [2] * nums2         
