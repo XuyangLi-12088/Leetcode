6914dcd7735fe48278c54173298f8a8445721da0
Xuyang Li <75594446+XuyangLi-12088@users.noreply.github.com>
2026-06-22 15:23:27 -0500

    Time: 111 ms (32.50%) | Memory: 34.9 MB (16.46%) - LeetSync

--- 739-daily-temperatures/daily-temperatures.py
+++ 739-daily-temperatures/daily-temperatures.py
@@ -1,21 +1,14 @@
 class Solution:
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
         # 单调递增栈
-        output = []
+        output = [0] * len(temperatures)
         stack = []
-        hash_map = {}
         for i, t in enumerate(temperatures):
             pop_n = None
             while stack and t > stack[-1][0]:
                 pop_n = stack.pop()
-                hash_map[pop_n[1]] = i
+                output[pop_n[1]] = i - pop_n[1]
             stack.append((t, i))
-
-        for i in range(len(temperatures)):
-            if i in hash_map:
-                output.append(hash_map[i] - i)
-            else:
-                output.append(0)
 
         return output
 
