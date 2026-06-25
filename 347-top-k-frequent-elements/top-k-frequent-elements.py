3bfdd8721d98e67592444b391e0d55f3a50bca34
Xuyang Li <75594446+XuyangLi-12088@users.noreply.github.com>
2026-06-24 23:50:00 -0500

    Time: 7 ms (51.93%) | Memory: 22.9 MB (42.12%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -1,26 +1,6 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        # # Hash Map + Max Heap Solution:
-        # result = []
-        # hash_map = {}
-        # for n in nums:
-        #     if n in hash_map:
-        #         hash_map[n] += 1
-        #     else:
-        #         hash_map[n] = 1
-            
-        # max_heap = []
-        # for key in hash_map:
-        #     max_heap.append((-hash_map[key], key))
-        # heapq.heapify(max_heap)
-
-        # for _ in range(k):
-        #     result.append(heapq.heappop(max_heap)[1])
-
-        # return result
-
-
-        # Hash Map + Min Heap Solution:
+        # Hash Map + Max Heap Solution:
         result = []
         hash_map = {}
         for n in nums:
@@ -29,16 +9,36 @@ class Solution:
             else:
                 hash_map[n] = 1
             
-        min_heap = []
-        heapq.heapify(min_heap)
+        max_heap = []
         for key in hash_map:
-            heapq.heappush(min_heap, (hash_map[key], key))
-            if len(min_heap) > k:
-                heapq.heappop(min_heap)
+            max_heap.append((-hash_map[key], key))
+        heapq.heapify(max_heap)
 
-        while min_heap:
-            result.append(heapq.heappop(min_heap)[1])
+        for _ in range(k):
+            result.append(heapq.heappop(max_heap)[1])
 
         return result
+
+
+        # # Hash Map + Min Heap Solution:
+        # result = []
+        # hash_map = {}
+        # for n in nums:
+        #     if n in hash_map:
+        #         hash_map[n] += 1
+        #     else:
+        #         hash_map[n] = 1
+            
+        # min_heap = []
+        # heapq.heapify(min_heap)
+        # for key in hash_map:
+        #     heapq.heappush(min_heap, (hash_map[key], key))
+        #     if len(min_heap) > k:
+        #         heapq.heappop(min_heap)
+
+        # while min_heap:
+        #     result.append(heapq.heappop(min_heap)[1])
+
+        # return result
 
 
