2df117699ae1799f2c31a3e1402f3eea94354319
Xuyang Li <75594446+XuyangLi-12088@users.noreply.github.com>
2026-06-24 23:05:55 -0500

    Time: 213 ms (23.81%) | Memory: 35.4 MB (25.24%) - LeetSync

---
+++ 239-sliding-window-maximum/sliding-window-maximum.py
@@ -0,0 +1,52 @@
+# Solution 1:
+class MyQueue:
+    def __init__(self):
+        self.queue = deque()
+
+    def pop(self, value):
+        if self.queue and value == self.queue[0]:
+            self.queue.popleft()
+
+    def push(self, value):
+        while self.queue and value > self.queue[-1]:
+            self.queue.pop()
+        self.queue.append(value)
+
+    def front(self):
+        return self.queue[0]
+
+class Solution:
+    def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
+        que = MyQueue()
+        result = []
+        for i in range(k):
+            que.push(nums[i])
+        result.append(que.front())
+        for i in range(k, len(nums)):
+            que.pop(nums[i - k])
+            que.push(nums[i])
+            result.append(que.front())
+        return result
+
+# Solution 2:
+# class Solution:
+#     def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
+#         max_list = []
+#         kept_nums = deque()
+
+#         for i in range(len(nums)):
+#             self.update_kept_nums(kept_nums, nums[i])
+
+#             if i >= k and nums[i - k] == kept_nums[0]:
+#                 kept_nums.popleft()
+
+#             if i >= k - 1:
+#                 max_list.append(kept_nums[0])
+
+#         return max_list
+
+#     def update_kept_nums(self, kept_nums, num):
+#         while kept_nums and num > kept_nums[-1]:
+#             kept_nums.pop()
+
+#         kept_nums.append(num)
