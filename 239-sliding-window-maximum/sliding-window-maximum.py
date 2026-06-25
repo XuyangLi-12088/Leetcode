8ef25c7a7d67f3fc10324e024bba4dcda2a5e839
Xuyang Li <75594446+XuyangLi-12088@users.noreply.github.com>
2026-06-24 23:22:43 -0500

    Time: 329 ms (12.80%) | Memory: 47.6 MB (5.09%) - LeetSync

--- 239-sliding-window-maximum/sliding-window-maximum.py
+++ 239-sliding-window-maximum/sliding-window-maximum.py
@@ -1,32 +1,46 @@
-# Solution 1:
-class MyQueue:
-    def __init__(self):
-        self.queue = deque()
-
-    def pop(self, value):
-        if self.queue and value == self.queue[0]:
-            self.queue.popleft()
-
-    def push(self, value):
-        while self.queue and value > self.queue[-1]:
-            self.queue.pop()
-        self.queue.append(value)
-
-    def front(self):
-        return self.queue[0]
-
+# 优先队列解法:
 class Solution:
     def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
-        que = MyQueue()
-        result = []
-        for i in range(k):
-            que.push(nums[i])
-        result.append(que.front())
-        for i in range(k, len(nums)):
-            que.pop(nums[i - k])
-            que.push(nums[i])
-            result.append(que.front())
-        return result
+        size = len(nums)
+        q = [(-nums[i], i) for i in range(k)]
+        heapq.heapify(q)
+        res = [-q[0][0]]
+        for i in range(k, size):
+            heapq.heappush(q, (-nums[i], i))
+            while q[0][1] <= i - k:
+                heapq.heappop(q)
+            res.append(-q[0][0])
+        return res
+
+# Solution 1:
+# class MyQueue:
+#     def __init__(self):
+#         self.queue = deque()
+
+#     def pop(self, value):
+#         if self.queue and value == self.queue[0]:
+#             self.queue.popleft()
+
+#     def push(self, value):
+#         while self.queue and value > self.queue[-1]:
+#             self.queue.pop()
+#         self.queue.append(value)
+
+#     def front(self):
+#         return self.queue[0]
+
+# class Solution:
+#     def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
+#         que = MyQueue()
+#         result = []
+#         for i in range(k):
+#             que.push(nums[i])
+#         result.append(que.front())
+#         for i in range(k, len(nums)):
+#             que.pop(nums[i - k])
+#             que.push(nums[i])
+#             result.append(que.front())
+#         return result
 
 # Solution 2:
 # class Solution:
