d2838e6ac2cc85118805b08604a23fd1a7620648
Xuyang Li <75594446+XuyangLi-12088@users.noreply.github.com>
2026-07-15 15:07:31 -0500

    Time: 2 ms (60.86%) | Memory: 19.4 MB (76.72%) - LeetSync

--- 1032-satisfiability-of-equality-equations/satisfiability-of-equality-equations.py
+++ 1032-satisfiability-of-equality-equations/satisfiability-of-equality-equations.py
@@ -4,7 +4,7 @@ class UnionFind:
 
     def find(self, x):  # 查找元素根节点的合集编号内部实现方式
         while self.fa[x] != x:  # 递归查找元素的父节点，直到根节点
-            self.fa[x] = self.fa[self.fa[x]]    # 隔代压缩优化
+            #self.fa[x] = self.fa[self.fa[x]]    # 隔代压缩优化
             x = self.fa[x]
         return x    # 返回元素根节点的集合编号
 
