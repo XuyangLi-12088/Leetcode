05c0c674e704d101280eee91d402adf5fb671b27
Xuyang Li <75594446+XuyangLi-12088@users.noreply.github.com>
2026-07-17 11:58:46 -0500

    Time: 11 ms (32.34%) | Memory: 19.6 MB (76.74%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.py
+++ 125-valid-palindrome/valid-palindrome.py
@@ -13,15 +13,15 @@ class Solution:
         #     return False
 
         # 相向双指针
-        s_list = list(s)
+        # s_list = list(s)
         left = 0
         right = len(s) - 1
         while left < right:
-            while left < right and not s_list[left].isalnum():
+            while left < right and not s[left].isalnum():
                 left += 1
-            while left < right and not s_list[right].isalnum():
+            while left < right and not s[right].isalnum():
                 right -= 1
-            if s_list[left].lower() != s_list[right].lower():
+            if s[left].lower() != s[right].lower():
                 return False
             left += 1
             right -= 1
