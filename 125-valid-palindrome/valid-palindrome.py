3a1cf4761f104de51000fd29672c5e3763bc2fc7
Xuyang Li <75594446+XuyangLi-12088@users.noreply.github.com>
2026-07-17 11:56:00 -0500

    Time: 11 ms (32.34%) | Memory: 20.2 MB (26.35%) - LeetSync

---
+++ 125-valid-palindrome/valid-palindrome.py
@@ -0,0 +1,32 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        # clean_s = ""
+        # for c in s:
+        #     if c.isalnum():
+        #         clean_s += c.lower()
+        
+        # backward_str = clean_s[::-1]
+
+        # if clean_s == backward_str:
+        #     return True
+        # else:
+        #     return False
+
+        # 相向双指针
+        s_list = list(s)
+        left = 0
+        right = len(s) - 1
+        while left < right:
+            while left < right and not s_list[left].isalnum():
+                left += 1
+            while left < right and not s_list[right].isalnum():
+                right -= 1
+            if s_list[left].lower() != s_list[right].lower():
+                return False
+            left += 1
+            right -= 1
+
+        return True
+
+
+        
