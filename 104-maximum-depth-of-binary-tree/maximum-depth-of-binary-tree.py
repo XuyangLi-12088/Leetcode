a83253e9ac75b141221d08dcd89096d71a805f1a
Xuyang Li <75594446+XuyangLi-12088@users.noreply.github.com>
2026-07-20 14:54:55 -0500

    Time: 0 ms (100.00%) | Memory: 22.5 MB (14.49%) - LeetSync

--- 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.py
+++ 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.py
@@ -14,21 +14,28 @@ class Solution:
         #     right = self.maxDepth(root.right)
         #     return max(left, right) + 1
 
-
-        # BFS Solution:
-        if not root:
+        if root is None:
             return 0
-        queue = deque()
-        queue.append(root)
-        result = 0
-        while queue:
-            cur_queue_size = len(queue)
-            for _ in range(cur_queue_size):
-                cur = queue.popleft()
-                if cur.left:
-                    queue.append(cur.left)
-                if cur.right:
-                    queue.append(cur.right)
-            result += 1
-        return result
+
+        left = self.maxDepth(root.left)
+        right = self.maxDepth(root.right)
+        return max(left, right) + 1
+        
+
+        # # BFS Solution:
+        # if not root:
+        #     return 0
+        # queue = deque()
+        # queue.append(root)
+        # result = 0
+        # while queue:
+        #     cur_queue_size = len(queue)
+        #     for _ in range(cur_queue_size):
+        #         cur = queue.popleft()
+        #         if cur.left:
+        #             queue.append(cur.left)
+        #         if cur.right:
+        #             queue.append(cur.right)
+        #     result += 1
+        # return result
         
