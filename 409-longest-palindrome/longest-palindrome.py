01e4350408c8d1c0421cb6f5d58a89546fac9b25
Xuyang Li <75594446+XuyangLi-12088@users.noreply.github.com>
2026-07-20 10:27:04 -0500

    Time: 3 ms (46.51%) | Memory: 19.5 MB (6.19%) - LeetSync

--- 409-longest-palindrome/longest-palindrome.py
+++ 409-longest-palindrome/longest-palindrome.py
@@ -3,8 +3,14 @@ class Solution:
         output = 0
         hash_map = collections.Counter(s)
         for l in hash_map:
-            output += (hash_map[l] // 2) * 2
-            hash_map[l] -= (hash_map[l] // 2) * 2
+            # 如果是偶数
+            if hash_map[l] % 2 == 0:
+                output += hash_map[l]
+                hash_map[l] -= hash_map[l]
+            # 如果是奇数
+            else:
+                output += hash_map[l] - hash_map[l] % 2
+                hash_map[l] -= hash_map[l] - hash_map[l] % 2
             
         for l in hash_map:
             if hash_map[l] != 0:
