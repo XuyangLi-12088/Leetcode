16614ed1f4bbd276c5befecbfe53ef9008f7eafe
Xuyang Li <75594446+XuyangLi-12088@users.noreply.github.com>
2026-07-20 09:49:20 -0500

    Time: 0 ms (100.00%) | Memory: 19.3 MB (16.65%) - LeetSync

---
+++ 70-climbing-stairs/climbing-stairs.py
@@ -0,0 +1,9 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        @cache
+        def dfs(i: int) -> int:
+            if i <= 1:  # 递归边界
+                return 1
+            return dfs(i - 1) + dfs(i - 2)
+        return dfs(n)
+        
