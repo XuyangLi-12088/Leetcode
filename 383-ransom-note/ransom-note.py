021c64f2aa091c51a784cc754e9ec33db2488c2c
Xuyang Li <75594446+XuyangLi-12088@users.noreply.github.com>
2026-07-20 09:32:14 -0500

    Time: 14 ms (80.11%) | Memory: 19.4 MB (99.09%) - LeetSync

--- 383-ransom-note/ransom-note.py
+++ 383-ransom-note/ransom-note.py
@@ -1,7 +1,7 @@
 class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
-        counter_r = Counter(ransomNote)
-        counter_m = Counter(magazine)
+        counter_r = collections.Counter(ransomNote)
+        counter_m = collections.Counter(magazine)
         for key in counter_r:
             if counter_r[key] > counter_m[key]:
                 return False
