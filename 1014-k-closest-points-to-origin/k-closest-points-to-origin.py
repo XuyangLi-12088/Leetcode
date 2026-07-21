bf49b888a94b57161ffa8c371c4d961fa4bd5004
Xuyang Li <75594446+XuyangLi-12088@users.noreply.github.com>
2026-07-21 18:08:30 -0500

    Time: 79 ms (26.38%) | Memory: 25.1 MB (33.01%) - LeetSync

---
+++ 1014-k-closest-points-to-origin/k-closest-points-to-origin.py
@@ -0,0 +1,16 @@
+class Solution:
+    def euc_distance(self, x: List[int], y: List[int]) -> int:
+        return ((x[0] - x[1]) ** 2 + (y[0] - y[1]) ** 2) ** 0.5
+
+    def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
+        output = []
+        heap = []
+        for i in range(len(points)):
+            heapq.heappush(heap, (self.euc_distance([points[i][0], 0], [points[i][1], 0]), i))
+
+        for _ in range(k):
+            output.append(points[heapq.heappop(heap)[1]])
+
+        return output
+
+        
