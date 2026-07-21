54ed8a1b39833640d0a3a8b514e5137df7018760
Xuyang Li <75594446+XuyangLi-12088@users.noreply.github.com>
2026-07-21 18:10:38 -0500

    Time: 86 ms (18.42%) | Memory: 25.6 MB (12.50%) - LeetSync

--- 1014-k-closest-points-to-origin/k-closest-points-to-origin.py
+++ 1014-k-closest-points-to-origin/k-closest-points-to-origin.py
@@ -6,7 +6,9 @@ class Solution:
         output = []
         heap = []
         for i in range(len(points)):
-            heapq.heappush(heap, (self.euc_distance([points[i][0], 0], [points[i][1], 0]), i))
+            heap.append((self.euc_distance([points[i][0], 0], [points[i][1], 0]), i))
+
+        heapq.heapify(heap)
 
         for _ in range(k):
             output.append(points[heapq.heappop(heap)[1]])
