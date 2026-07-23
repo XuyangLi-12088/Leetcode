10f7c86072bb65d68fdcfe6e93fc911d20e1724f
Xuyang Li <75594446+XuyangLi-12088@users.noreply.github.com>
2026-07-22 23:50:54 -0500

    Time: 22 ms (57.60%) | Memory: 25.5 MB (70.20%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.py
+++ 238-product-of-array-except-self/product-of-array-except-self.py
@@ -1,33 +1,31 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
         n = len(nums)
-        pre = [1] * n
         suf = [1] * n
-        for i in range(1, n):
-            pre[i] = pre[i - 1] * nums[i - 1]
-
         for i in range(n - 2, -1, -1):
             suf[i] = suf[i + 1] * nums[i + 1]
 
-        ans = []
-        for i in range(n):
-            ans.append(pre[i] * suf[i])
+        pre = 1
+        for i, x in enumerate(nums):
+            suf[i] *= pre
+            pre *= x
 
-        return ans
+        return suf
 
-        # output = [1] * len(nums)
-        
-        # left = 1
-        # for i in range(len(nums)):
-        #     output[i] *= left
-        #     left *= nums[i]
-        
-        # right = 1
-        # for i in range(len(nums) - 1, -1, -1):
-        #     output[i] *= right
-        #     right *= nums[i]
-    
-        # return output 
+        # n = len(nums)
+        # pre = [1] * n
+        # suf = [1] * n
+        # for i in range(1, n):
+        #     pre[i] = pre[i - 1] * nums[i - 1]
+
+        # for i in range(n - 2, -1, -1):
+        #     suf[i] = suf[i + 1] * nums[i + 1]
+
+        # ans = []
+        # for i in range(n):
+        #     ans.append(pre[i] * suf[i])
+
+        # return ans
         
 
 
