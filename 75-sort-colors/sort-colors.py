bcbe83587f9c9e254460043b4227de2dda4dea7c
Xuyang Li <75594446+XuyangLi-12088@users.noreply.github.com>
2026-08-11 18:05:53 -0500

    Time: 0 ms (100.00%) | Memory: 19.3 MB (22.34%) - LeetSync

--- 75-sort-colors/sort-colors.py
+++ 75-sort-colors/sort-colors.py
@@ -3,39 +3,24 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        red = white = blue = 0
-        for x in nums:
-            if x == 0:
-                red += 1
-            if x == 1:
-                white += 1
-            if x == 2:
-                blue += 1
-        nums[:red] = [0] * red
-        nums[red:red+white] = [1] * white
-        nums[red+white:] = [2] * blue
-
-
-
-        # p0 = p1 = 0
-        # for i, x in enumerate(nums):
-        #     nums[i] = 2
-        #     if x <= 1:
-        #         nums[p1] = 1
-        #         p1 += 1
-        #     if x == 0:
-        #         nums[p0] = 0
-        #         p0 += 1
-
-        # nums0 = nums1 = nums2 = 0
+        # red = white = blue = 0
         # for x in nums:
         #     if x == 0:
-        #         nums0 += 1
-        #     elif x == 1:
-        #         nums1 += 1
-        #     elif x == 2:
-        #         nums2 += 1
+        #         red += 1
+        #     if x == 1:
+        #         white += 1
+        #     if x == 2:
+        #         blue += 1
+        # nums[:red] = [0] * red
+        # nums[red:red+white] = [1] * white
+        # nums[red+white:] = [2] * blue
 
-        # nums[:nums0] = [0] * nums0
-        # nums[nums0:nums0+nums1] = [1] * nums1
-        # nums[nums0+nums1:] = [2] * nums2         
+        p0 = p1 = 0
+        for i, x in enumerate(nums):
+            nums[i] = 2 
+            if x <= 1:
+                nums[p1] = 1
+                p1 += 1
+            if x == 0:
+                nums[p0] = 0
+                p0 += 1
