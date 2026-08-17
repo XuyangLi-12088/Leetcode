0521a7596e807c2207146e1f8a52c6b72c08390d
Xuyang Li <75594446+XuyangLi-12088@users.noreply.github.com>
2026-08-17 18:04:47 -0500

    Time: 0 ms (100.00%) | Memory: 19.4 MB (24.03%) - LeetSync

--- 198-house-robber/house-robber.py
+++ 198-house-robber/house-robber.py
@@ -1,8 +1,20 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        f0 = f1 = 0
-        for i, x in enumerate(nums):
-            new_f = max(f1, f0 + x)
-            f0 = f1
-            f1 = new_f
-        return f1
+        # f0 = f1 = 0
+        # for i, x in enumerate(nums):
+        #     new_f = max(f1, f0 + x)
+        #     f0 = f1
+        #     f1 = new_f
+        # return f1
+
+
+        n = len(nums)
+        @cache
+        def dfs(i):
+            if i < 0:
+                return 0
+            return max(dfs(i-1), dfs(i-2) + nums[i])
+            
+        return dfs(n-1)
+
+        
