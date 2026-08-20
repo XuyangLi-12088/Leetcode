3734c6adb5a4dec01f795fd739048dc706e644d6
Xuyang Li <75594446+XuyangLi-12088@users.noreply.github.com>
2026-08-19 23:46:24 -0500

    Time: 139 ms (50.55%) | Memory: 79.7 MB (6.25%) - LeetSync

--- 494-target-sum/target-sum.py
+++ 494-target-sum/target-sum.py
@@ -1,8 +1,9 @@
 class Solution:
     def findTargetSumWays(self, nums: List[int], target: int) -> int:
         size = len(nums)
-        table = {}
+        # table = {}
 
+        @cache
         def dfs(i, cur_sum):
             if i == size:
                 if cur_sum == target:
@@ -10,11 +11,11 @@ class Solution:
                 else:
                     return 0
 
-            if (i, cur_sum) in table:
-                return table[(i, cur_sum)]
+            # if (i, cur_sum) in table:
+            #     return table[(i, cur_sum)]
 
             cnt = dfs(i + 1, cur_sum - nums[i]) + dfs(i + 1, cur_sum + nums[i])
-            table[(i, cur_sum)] = cnt
+            # table[(i, cur_sum)] = cnt
             return cnt
 
         return dfs(0, 0)
