3e75895678fd56b6141bec8c3c19f395ecca7ceb
Xuyang Li <75594446+XuyangLi-12088@users.noreply.github.com>
2026-08-25 11:29:17 -0500

    Time: 2 ms (23.50%) | Memory: 20.1 MB (7.54%) - LeetSync

--- 62-unique-paths/unique-paths.py
+++ 62-unique-paths/unique-paths.py
@@ -1,5 +1,37 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
+        @cache
+        def dfs(x, y):
+            if x == m-1 and y == n-1:
+                return 1
+            if x == m-1:
+                return dfs(x, y+1)
+            if y == n-1:
+                return dfs(x+1, y)
+            return dfs(x+1, y) + dfs(x, y+1)
+
+        return dfs(0, 0)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         dp = [[0 for _ in range(n)] for _ in range(m)]
 
         for j in range(n):
