e1cd1885a5394952eaadb8304452f6016bbd7d1c
Xuyang Li <75594446+XuyangLi-12088@users.noreply.github.com>
2026-08-31 15:19:55 -0500

    Time: 0 ms (100.00%) | Memory: 19.4 MB (40.69%) - LeetSync

--- 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.py
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.py
@@ -1,20 +1,21 @@
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
-        mapping = ["", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"]
+        digit_list = ["", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"]
         n = len(digits)
         if n == 0:
             return []
 
-        ans = []
-        path = [""] * n
+        output = []
+        path = []
         def dfs(i):
             if i == n:
-                ans.append("".join(path))
+                output.append("".join(path.copy()))
                 return
-            else:
-                for c in mapping[int(digits[i])]:
-                    path[i] = c
-                    dfs(i + 1)
+
+            for c in digit_list[int(digits[i])]:
+                path.append(c)
+                dfs(i+1)
+                path.pop()
 
         dfs(0)
-        return ans
+        return output
