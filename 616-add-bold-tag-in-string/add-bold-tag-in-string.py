aba425c05e823e3f945f77e8069e052f516a51bc
Xuyang Li <75594446+XuyangLi-12088@users.noreply.github.com>
2026-09-02 00:00:01 -0500

    Time: 999 ms (6.33%) | Memory: 19.4 MB (87.33%) - LeetSync

---
+++ 616-add-bold-tag-in-string/add-bold-tag-in-string.py
@@ -0,0 +1,33 @@
+class Solution:
+    def addBoldTag(self, s: str, words: List[str]) -> str:
+        bold = [0] * len(s)
+        word_set = set(words)
+        max_len = 0
+        for w in words:
+            max_len = max(max_len, len(w))
+
+        for i in range(len(s)):
+            for j in range(0, max_len):
+                if i+j+1 <= len(s):
+                    cur_substr = s[i:i+j+1]
+                    if cur_substr in word_set:
+                        bold[i:i+j+1] = [1] * (j+1)
+
+        output = ""
+        flag = 0
+        for i in range(len(bold)):
+            if bold[i] == 0 and flag == 1:
+                output += "</b>"
+                output += s[i]
+                flag = 0
+            elif bold[i] == 1 and flag == 0:
+                output += "<b>"
+                output += s[i]
+                flag = 1
+            else:
+                output += s[i]
+
+        if flag == 1:
+            output += "</b>"
+
+        return output
