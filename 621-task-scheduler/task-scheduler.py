fa436ff8e45c3026f919126c06056cdcf1f97045
Xuyang Li <75594446+XuyangLi-12088@users.noreply.github.com>
2026-09-10 10:37:16 -0500

    Time: 179 ms (20.88%) | Memory: 20.9 MB (15.79%) - LeetSync

--- 621-task-scheduler/task-scheduler.py
+++ 621-task-scheduler/task-scheduler.py
@@ -1,22 +1,24 @@
 class Solution:
     def leastInterval(self, tasks: List[str], n: int) -> int:
         cnt = collections.Counter(tasks)
-        max_heap = [-c for c in cnt.values()]
+        max_heap = [(-cnt[k], k) for k in cnt]
         heapq.heapify(max_heap)
 
         time = 0
         cooldown = collections.deque()
+        output = []
         while max_heap or cooldown:
             time += 1
 
-            if cooldown and cooldown[0][1] == time:
-                heapq.heappush(max_heap, cooldown.popleft()[0])
+            if cooldown and cooldown[0][2] == time:
+                r, k, _ = cooldown.popleft()
+                heapq.heappush(max_heap, (r, k))
 
             if max_heap:
-                remaining = heapq.heappop(max_heap) + 1
-                if remaining:
-                    cooldown.append((remaining, time + n + 1))
+                c, k = heapq.heappop(max_heap)
+                remaining = c + 1
+                if remaining != 0:
+                    output.append(k)
+                    cooldown.append((remaining, k, time + n + 1))
             
         return time
-            
-
