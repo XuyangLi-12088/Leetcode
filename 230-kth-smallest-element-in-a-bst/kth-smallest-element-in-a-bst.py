c2498ffb03a9291115bbd7b442f0be2a125138b7
Xuyang Li <75594446+XuyangLi-12088@users.noreply.github.com>
2026-09-10 16:31:04 -0500

    Time: 3 ms (28.60%) | Memory: 22.5 MB (11.50%) - LeetSync

--- 230-kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst.py
+++ 230-kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst.py
@@ -6,15 +6,15 @@
 #         self.right = right
 class Solution:
     def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
-        def dfs(node: Optional[TreeNode]):
-            if node is None:
-                return -1   # 题目保证节点值非负，用 -1 表示没有找到
-            left_res = dfs(node.left)
-            if left_res != -1:  # 答案在左子树中
-                return left_res
-            nonlocal k
-            k -= 1
-            if k == 0:  # 答案就是当前节点
-                return node.val
-            return dfs(node.right)  # 右子树会返回答案或者 -1
-        return dfs(root)
+        output = []
+        def dfs(node):
+            if node == None:
+                return
+            dfs(node.left)
+            output.append(node.val)
+            dfs(node.right)
+            return
+
+        dfs(root)
+        print(output)
+        return output[k-1]
