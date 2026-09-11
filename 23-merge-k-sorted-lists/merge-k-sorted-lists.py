5357e9c27a5657ad109f1e7cc47f5043833e65bf
Xuyang Li <75594446+XuyangLi-12088@users.noreply.github.com>
2026-09-11 16:16:25 -0500

    Time: 7 ms (82.71%) | Memory: 23 MB (29.44%) - LeetSync

--- 23-merge-k-sorted-lists/merge-k-sorted-lists.py
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.py
@@ -10,18 +10,16 @@ class Solution:
         min_heap = []
         for i, l in enumerate(lists):
             if l:
-                heapq.heappush(min_heap, (l.val, i))
+                heapq.heappush(min_heap, (l.val, i, l))
                 lists[i] = l.next
         while len(min_heap) != 0:
             pop = heapq.heappop(min_heap)
-            # create New Node
-            new_node = ListNode(pop[0])
             # add to the one sorted linked list
-            cur.next = new_node
+            cur.next = pop[2]
             cur = cur.next
             # add the next node into min heap
             if lists[pop[1]]:
-                heapq.heappush(min_heap, (lists[pop[1]].val, pop[1]))
+                heapq.heappush(min_heap, (lists[pop[1]].val, pop[1], lists[pop[1]]))
                 lists[pop[1]] = lists[pop[1]].next
 
         return dummy.next
