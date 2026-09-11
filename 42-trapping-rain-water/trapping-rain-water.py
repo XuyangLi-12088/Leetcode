de4a6863c6175db741fe550ee9cd52286723fd34
Xuyang Li <75594446+XuyangLi-12088@users.noreply.github.com>
2026-09-11 17:11:00 -0500

    Time: 7 ms (71.63%) | Memory: 21.3 MB (11.58%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.py
+++ 42-trapping-rain-water/trapping-rain-water.py
@@ -1,31 +1,37 @@
 class Solution:
     def trap(self, height: List[int]) -> int:
-        # [0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1]
-        left: 7
-        right: 7
-        left_h: 2
-        right_h: 2
-        ans: 1 + 1 + 2 + 1 + 1
-
         left = 0
         left_h = 0 # 储存left指针左面的历史最大高度
         right = len(height) - 1
         right_h = 0 # 储存right指针右面的历史最大高度
         ans = 0
         while left < right:
+            # height[left]和height[right]哪边小收缩哪个指针
             if height[left] <= height[right]:
+                # 如果当前高度大于历史最大高度，更新历史最大高度
                 if height[left] > left_h:
                     left_h = height[left]
+                # 如果当前高度小于历史最大高度，更新答案
                 else:
                     ans += left_h - height[left]
                 left += 1
             else:
+                # 如果当前高度大于历史最大高度，更新历史最大高度
                 if height[right] > right_h:
                     right_h = height[right]
+                # 如果当前高度小于历史最大高度，更新答案
                 else:
                     ans += right_h - height[right]
                 right -= 1
 
         return ans
+
+
+        # [0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1]
+        # left: 7
+        # right: 7
+        # left_h: 2
+        # right_h: 2
+        # ans: 1 + 1 + 2 + 1 + 1
 
 
