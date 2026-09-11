9a4608d6dc886035285d5a51ef92a23fffe59480
Xuyang Li <75594446+XuyangLi-12088@users.noreply.github.com>
2026-09-10 22:32:21 -0500

    Time: 43 ms (96.97%) | Memory: 19.8 MB (37.58%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.py
+++ 76-minimum-window-substring/minimum-window-substring.py
@@ -1,18 +1,35 @@
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
-        cnt_s = collections.Counter()
-        cnt_t = collections.Counter(t)
+        diff = {}   # 窗口每种字母个数 - t 每种字母个数
+        for c in t:
+            if c in diff:
+                diff[c] -= 1
+            else:
+                diff[c] = -1
+        kinds = len(diff)
 
         ans_left, ans_right = -1, len(s)
         left = 0
+        ge_cnt = 0  # 窗口内有 ge_cnt 种字母的出现次数 >= t 中相应字母的出现次数
 
         for right, c in enumerate(s):
-            cnt_s[c] += 1
-            while cnt_s >= cnt_t:
-                if right - left < ans_right - ans_left:
-                    ans_left, ans_right = left, right
-                cnt_s[s[left]] -= 1
-                left += 1
+            if c in diff:
+                diff[c] += 1
+                if diff[c] == 0:
+                    ge_cnt += 1
 
-        return "" if ans_left == -1 else s[ans_left : ans_right+1]
+                while ge_cnt == kinds:
+                    if right - left < ans_right - ans_left:
+                        ans_left, ans_right = left, right
+
+                    if s[left] in diff:
+                        if diff[s[left]] == 0:
+                            ge_cnt -= 1
+                        diff[s[left]] -= 1
+                    left += 1
+
+        return "" if ans_left == -1 else s[ans_left : ans_right + 1]
+
+
+        
 
