89d07ac48edd3b079c1cb9c29672738a9d1bd213
Xuyang Li <75594446+XuyangLi-12088@users.noreply.github.com>
2026-09-11 22:27:31 -0500

    Time: 155 ms (5.05%) | Memory: 28.1 MB (79.69%) - LeetSync

--- 739-daily-temperatures/daily-temperatures.py
+++ 739-daily-temperatures/daily-temperatures.py
@@ -1,14 +1,15 @@
 class Solution:
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
-        # 单调递增栈
+        # 单调栈
         output = [0] * len(temperatures)
-        stack = []
-        for i, t in enumerate(temperatures):
-            pop_n = None
-            while stack and t > stack[-1][0]:
-                pop_n = stack.pop()
-                output[pop_n[1]] = i - pop_n[1]
-            stack.append((t, i))
+        mono_stack = []
+        for i in range(len(temperatures)-1, -1, -1):
+            if len(mono_stack) != 0:
+                while len(mono_stack) != 0 and mono_stack[-1][0] <= temperatures[i]:
+                    mono_stack.pop()
+                if len(mono_stack) != 0:
+                    output[i] = mono_stack[-1][1] - i
 
+            # 加入单调栈
+            mono_stack.append((temperatures[i], i))
         return output
-
